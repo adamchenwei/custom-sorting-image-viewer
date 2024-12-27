d2f4fc8e797ebb8417f4b1bcba6f18a59fdc5912
Adam Chen Wei <adamchenwei@gmail.com>
2024-12-26 22:21:29 -0500

    build(): setup jest for tests main

---
+++ jest.setup.ts
@@ -0,0 +1,36 @@
+// jest.setup.ts
+import React from 'react';
+import '@testing-library/jest-dom';
+
+// Mock Next.js router
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+      back: jest.fn(),
+      forward: jest.fn(),
+    };
+  },
+  usePathname() {
+    return '';
+  },
+  useSearchParams() {
+    return new URLSearchParams();
+  },
+}));
+
+// Mock Next.js Image component
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: function MockImage(props: React.ImgHTMLAttributes<HTMLImageElement>) {
+    // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
+    return React.createElement('img', { ...props });
+  },
+}));
+
+// Clean up after each test
+afterEach(() => {
+  jest.clearAllMocks();
+});
